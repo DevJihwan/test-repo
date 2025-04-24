09a5a8a29b2cb7a2e7a91e8b4f07662d4c36b9cb
DevJihwan <108052941+DevJihwan@users.noreply.github.com>
2025-04-24 14:22:34 +0900

    Create src/App.js for React framework

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the test-repo React App!</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <footer className="App-footer">
+          <p>React Application - DevJihwan</p>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
